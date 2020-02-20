4170bfea342dddbb301d38d6727519288f7260b3
thudsonbu <hudson_thom@bentley.edu>
2020-02-20 17:07:40 -0500

    Documentation improvements

--- test.py
+++ test.py
@@ -12,6 +12,7 @@ def main():
                  " ", " ", " ",
                  " ", " ", " "]
 
+        # loopes through player turns until win or board full
         game_loop(board,player1_mark,player2_mark)
 
         # querry keep playing
@@ -28,7 +29,7 @@ def main():
     # bye
     print("\nThanks for playing!")
 
-
+ # prints the game board
 def print_board(dictionary):
     print("\nBoard Display: \n")
     # print first line of board
@@ -47,7 +48,7 @@ def print_board(dictionary):
         print(dictionary[i], end=" | ")
     print()
 
-
+# a player turn for marking board
 def player_turn(game_board,mark):
     print("\nPlayer " + mark + "'s turn.")
     while True:
@@ -68,8 +69,9 @@ def player_turn(game_board,mark):
         else:
             print("\nThat space is already taken.")
 
-
+# checks a game board if win found for specific mark
 def check_win(game_board, mark):
+    # combinations of marks that cause a win
     win_combinations = [[1, 2, 3],
                         [4, 5, 6],
                         [7, 8, 9],
@@ -95,6 +97,7 @@ def check_win(game_board, mark):
             combination_found = True
     return combination_found
 
+# introduction to the game that returns player's chosen markers
 def intro_get_markers():
     print("\nWelcome to Tom's Tic Tac Toe.")
     while True:
@@ -113,6 +116,7 @@ def intro_get_markers():
     print("\nPlayer2's marker is " + player2_mark)
     return player1_mark, player2_mark
 
+# loops through player turns until win or board it full (tie)
 def game_loop(game_board,player1_mark,player2_mark):
     round = 1
     while(True):
@@ -122,7 +126,7 @@ def game_loop(game_board,player1_mark,player2_mark):
             print_board(game_board)
             break
         round += 1
-        if round > 9:
+        if round > 9: # check is in the middle since last round is round 9 (odd)
             print("\n!!! TIE !!!")
             print_board(game_board)
             break
